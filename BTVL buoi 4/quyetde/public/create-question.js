d2f043be9446553ca509d617d3316b75e2fd876f
kien2929 <kien2929@gmail.com>
2019-03-06 19:17:59 +0700

    BTVN buoi 5 fix1

--- BTVL buoi 4/quyetde/public/create-question.js
+++ BTVL buoi 4/quyetde/public/create-question.js
@@ -12,7 +12,7 @@ $(document).ready(() => {
     const pathname = window.location.search;
     console.log(pathname);
   $.ajax({
-    url: 'get-create${pathname}',
+    url: `get-create${pathname}`,
     type: 'GET',
     success: (data) => {},
     error: (err) => {
