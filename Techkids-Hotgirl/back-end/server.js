c5038806ab5e2da86b9943add5ae66023eef72d6
kien2929 <kien2929@gmail.com>
2019-04-03 00:40:51 +0700

    add front

--- Techkids-Hotgirl/back-end/server.js
+++ Techkids-Hotgirl/back-end/server.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/hot-girl',(err)=>{
     if(err) throw err;
     const app = express();
     //midle ware
+    app.use(express.static('public'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false}));
     app.use(expressSession({
@@ -25,7 +26,19 @@ app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);   
 app.use('/api/auth',authRouter); 
 
+app.get('/sign-in',(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname + '/public/sign-in.html'));
+});
 
+//register
+app.get('/register',(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname + '/public/register.html'));
+});
+
+//create-post
+app.get('/posts',(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname + '/create-posts/index.html'));
+});
 // start server
     app.listen(3000,(err)=>{
     if (err) throw err;
